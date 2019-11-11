5304216cb23e017f7c9ebd514e31f60adc99e1df
sifu.tang <sifu.tang@tinno.com>
2019-11-11 10:16:57 +0800

    request camera permission

--- app/src/main/java/com/example/cameraxdemo/MainActivity.kt
+++ app/src/main/java/com/example/cameraxdemo/MainActivity.kt
@@ -1,22 +1,26 @@
 package com.example.cameraxdemo
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.graphics.SurfaceTexture
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.Surface
 import android.view.TextureView
 import androidx.camera.core.CameraX
 import androidx.camera.core.Preview
 import androidx.camera.core.PreviewConfig
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
 
     companion object {
-        const val TAG = "MainActivity"
+        private const val TAG = "MainActivity"
+        private const val REQUEST_CAMERA = 1
     }
 
-    private val textureView:TextureView by lazy {
+    private val mTextureView:TextureView by lazy {
         findViewById<TextureView>(R.id.textureView)
     }
 
@@ -31,61 +35,81 @@ class MainActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
 
-        val previewConfig = PreviewConfig.Builder()
-                .setLensFacing(CameraX.LensFacing.FRONT)
-                .build()
-        val preview = Preview(previewConfig)
-        preview.setOnPreviewOutputUpdateListener(Preview.OnPreviewOutputUpdateListener {
-//            textureView.surfaceTexture = it.surfaceTexture
-
-
-            textureView.surfaceTextureListener = object : TextureView.SurfaceTextureListener {
-                override fun onSurfaceTextureSizeChanged(surface: SurfaceTexture?, width: Int, height: Int) {
-                    Log.d(TAG, "onSurfaceTextureSizeChanged: width = $width, height = $height")
-                }
-
-                override fun onSurfaceTextureUpdated(surface: SurfaceTexture?) {
-
-                }
-
-                override fun onSurfaceTextureDestroyed(surface: SurfaceTexture?): Boolean {
-                    Log.d(TAG, "onSurfaceTextureDestroyed: texture release...")
-                    return true
-                }
-
-                override fun onSurfaceTextureAvailable(surface: SurfaceTexture?, width: Int, height: Int) {
-                    Log.d(TAG, "onSurfaceTextureAvailable: texture available")
-                    mOESTextureId = TextureDrawer.createOESTextureObject()
-                    mRender.init(textureView, mOESTextureId)
-                    mRender.initOESTexture(it.surfaceTexture)
-                }
-            }
-
-
-            // Compute the center of preview (TextureView)
-            val centerX = textureView.width.toFloat().div(2)
-            val centerY = textureView.height.toFloat().div(2)
-
-            // Correct preview output to account for display rotation
-            val rotationDegrees = when (textureView.display?.rotation) {
-                Surface.ROTATION_0 -> 0
-                Surface.ROTATION_90 -> 90
-                Surface.ROTATION_180 -> 180
-                Surface.ROTATION_270 -> 270
-                else -> return@OnPreviewOutputUpdateListener
-            }
-
-            val matrix = android.graphics.Matrix()
-            matrix.postRotate(-rotationDegrees.toFloat(), centerX, centerY)
-
-            // Finally, apply transformations to TextureView
-            textureView.setTransform(matrix)
-        })
-        CameraX.bindToLifecycle(this, preview)
+        if (!hasCameraPermission()) {
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.CAMERA), REQUEST_CAMERA)
+        } else {
+            startCamera()
+        }
     }
 
     override fun onPause() {
         super.onPause()
         CameraX.unbindAll()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == REQUEST_CAMERA && grantResults.size == 1
+            && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            Log.i(TAG, "onRequestPermissionsResult: permission camera PERMISSION_GRANTED")
+            if (mTextureView.isAvailable) {
+                Log.e(TAG, "onRequestPermissionsResult: ")
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
+    }
+
+    private fun hasCameraPermission(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            this, Manifest.permission.CAMERA)== PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun startCamera() {
+        Log.i(TAG, "startCamera: ")
+        val previewConfig = PreviewConfig.Builder()
+            .setLensFacing(CameraX.LensFacing.FRONT)
+            .build()
+        val preview = Preview(previewConfig)
+        preview.setOnPreviewOutputUpdateListener(Preview.OnPreviewOutputUpdateListener {
+            //            textureView.surfaceTexture = it.surfaceTexture
+            Log.d(TAG, "setOnPreviewOutputUpdateListener mTextureView.isAvailable = ${mTextureView.isAvailable}")
+            if (mTextureView.isAvailable) {
+                startGlContext(it.surfaceTexture)
+            } else{
+                mTextureView.surfaceTextureListener = object : TextureView.SurfaceTextureListener {
+                    override fun onSurfaceTextureSizeChanged(surface: SurfaceTexture?, width: Int, height: Int) {
+                        Log.d(TAG, "onSurfaceTextureSizeChanged: width = $width, height = $height")
+                    }
+
+                    override fun onSurfaceTextureUpdated(surface: SurfaceTexture?) {
+
+                    }
+
+                    override fun onSurfaceTextureDestroyed(surface: SurfaceTexture?): Boolean {
+                        Log.d(TAG, "onSurfaceTextureDestroyed: texture release...")
+                        return true
+                    }
+
+                    override fun onSurfaceTextureAvailable(surface: SurfaceTexture?, width: Int, height: Int) {
+                        Log.d(TAG, "onSurfaceTextureAvailable: texture available")
+                        startGlContext(it.surfaceTexture)
+                    }
+                }
+            }
+        })
+        CameraX.bindToLifecycle(this, preview)
+    }
+
+    private fun startGlContext(surfaceTexture: SurfaceTexture) {
+        if (mOESTextureId == -1) {
+            mOESTextureId = TextureDrawer.createOESTextureObject()
+            mRender.init(mTextureView, mOESTextureId)
+            mRender.initOESTexture(surfaceTexture)
+        }
     }
 }
